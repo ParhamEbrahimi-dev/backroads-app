ab0884a04597fa1292e56935fb67ad769acc71b1
ParhamEbrahimi-dev <ParhamEbrahimi.dev@gmail.com>
2025-09-20 23:16:51 +0330

    Footer

--- src/data.ts
+++ src/data.ts
@@ -1,4 +1,8 @@
 import { title } from "process"
+import tour1 from "./images/tour-1.jpeg";
+import tour2 from "./images/tour-2.jpeg";
+import tour3 from "./images/tour-3.jpeg";
+import tour4 from "./images/tour-4.jpeg";
 
 export const pageLinks = [
     {id:1,href:"#home",text:" home "},
@@ -11,7 +15,6 @@ export const socialLinks=[
     {id:1,href:'https://www.twitter.com',icon:'fab fa-facebook'},
     {id:1,href:'https://www.twitter.com',icon:'fab fa-twitter'},
     {id:1,href:'https://www.twitter.com',icon:'fab fa-squarespace'}
-
 ]
 
 export const services = [
@@ -23,4 +26,59 @@ export const services = [
     {id:3,icon:"fas fa-socks fa-fw",title:"amazing comfort" ,text:`Lorem ipsum dolor sit amet consectetur adipisicing elit
         .Asperiores, officia`},
 
+]
+
+export const tours =[
+    {
+        id:1,
+        img:tour1,
+        date:"august 26th, 2020",
+        title:"Tibet Adventure",
+        info: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque
+                            vitae tempore voluptatum maxime reprehenderit eum quod
+                            exercitationem fugit, qui corporis.`,
+        duration:"6",
+        location:"china",
+        cost:"2100",
+
+    },
+    {
+        id:2,
+        img:tour2,
+        date:"october 1th, 2020",
+        title:"best of java",
+        info:`Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque
+                            vitae tempore voluptatum maxime reprehenderit eum quod
+                            exercitationem fugit, qui corporis.`,
+        duration:"11",
+        location:"indonesia",
+        cost:"1400",
+
+    },
+    {
+        id:3,
+        img:tour3,
+        date:"september 15th, 2020",
+        title:"explore hong kong",
+        info:`  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque
+                            vitae tempore voluptatum maxime reprehenderit eum quod
+                            exercitationem fugit, qui corporis.`,
+        duration:"8",
+        location:"hong kong",
+        cost:"5000",
+
+    },
+    {
+        id:4,
+        img:tour4,
+        date:"december 5th, 2019",
+        title:"kenya highlights",
+        info:`Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque
+                            vitae tempore voluptatum maxime reprehenderit eum quod
+                            exercitationem fugit, qui corporis.`,
+        duration:"20",
+        location:"kenya",
+        cost:"3300",
+
+    }
 ]
